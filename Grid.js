a61011dd4719c78c3b4c181a9e480118ace1572a
serhatcigerci <yserhatcigerci@gmail.com>
2022-04-25 21:02:26 +0300

    random cells start and private variables

--- Grid.js
+++ Grid.js
@@ -3,11 +3,38 @@ const CELL_SIZE = 20
 const CELL_GAP = 2
 
 export default class Grid {
+  #cells
   constructor(gridElem) {
     gridElem.style.setProperty('--grid-size', GRID_SIZE);
     gridElem.style.setProperty('--cell-size', `${CELL_SIZE}vmin`)
-    gridElem.style.setProperty('--cell-gap', `${CELL_GAP}vmin`);
-    createCellElements(gridElem)
+    gridElem.style.setProperty('--cell-gap', `${CELL_GAP}vmin`)
+    this.#cells = createCellElements(gridElem).map((cellElem, index)=> {
+      return new Cell(cellElem, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
+    })
+  }
+
+  get #emptyCells() {
+    return this.cells.filter(cell => cell.tile == null)
+  }
+  randomEmptyCell() {
+    const randomIndex = Math.floor(Math.random() * this.#emptyCells.length)
+    return this.#emptyCells[randomIndex]
+  }
+}
+
+class Cell {
+  #cellElem
+  #x
+  #y
+  #tile
+  constructor(cellElem, x, y) {
+    this.#cellElem = cellElem
+    this.#x = x
+    this.#y = y
+  }
+
+  get tile() {
+    return this.#tile
   }
 }
 
--- script.js
+++ script.js
@@ -3,3 +3,5 @@ import Grid from './Grid.js'
 const gameBoard = document.getElementById('game-board')
 
 const grid = new Grid(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard)
+grid.randomEmptyCell().tile = new Tile(gameBoard) 
